5c5e1f0d4ee0ad14a0550047fafc09416964529b
izumra <zagumennikovmark@gmail.com>
2023-09-26 21:53:05 +0500

    fix handlers

--- backend/main.go
+++ backend/main.go
@@ -6,22 +6,16 @@ import (
 	"github.com/Izumra/OwnGame/core/ws"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-	engine := html.New("../public", ".html")
 	app := fiber.New(fiber.Config{
 		ServerHeader: "Own game",
-		Views:        engine,
 	})
 	ws.SetWebSocket(app)
 	app.Use(cors.New())
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", nil)
-	})
-	app.Get("/view", func(c *fiber.Ctx) error {
-		return c.Render("view", nil)
+	app.Static("/", "../public", fiber.Static{
+		Browse: true,
 	})
 	log.Fatal(app.Listen(":8080"))
 }
