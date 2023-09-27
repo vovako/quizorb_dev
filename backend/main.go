d0f081763a3efba1d3cbe34c354683f1c538164d
izumra <zagumennikovmark@gmail.com>
2023-09-27 21:28:31 +0500

    test

--- backend/main.go
+++ backend/main.go
@@ -15,5 +15,11 @@ func main() {
 	ws.SetWebSocket(app)
 	app.Use(cors.New())
 	app.Static("/", "../public")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("../public/index.html")
+	})
+	app.Get("/view", func(c *fiber.Ctx) error {
+		return c.SendFile("../public/view.html")
+	})
 	log.Fatal(app.Listen(":8080"))
 }
