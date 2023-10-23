a508ec29ce285b594987b4c876d2eaf9843cbf2b
izumra <zagumennikovmark@gmail.com>
2023-10-24 00:38:49 +0500

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ import viewPage from './viewPage.js'
 const wsOrigin1 = 'wss://game-zmark.p.tnnl.in'
 const wsOrigin2 = 'ws://localhost:8080'
 const wsOrigin3='ws://192.168.147.66'
-const ws = new WebSocket(`${wsOrigin3}/websocket/connection`)
+const ws = new WebSocket(`${wsOrigin2}/websocket/connection`)
 
 switch (location.pathname) {
 	case '/':
