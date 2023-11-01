4829762aaa12f1d0952600bb56957523b1846dcc
vovako <vladimirkondaurov38@gmail.com>
2023-11-01 22:46:12 +0500

    login form

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,8 @@ import viewPage from './viewPage.js'
 const wsOrigin1 = 'wss://game-zmark.p.tnnl.in'
 const wsOrigin2 = 'ws://localhost:8080'
 const wsOrigin3 = 'ws://192.168.147.66'
-const ws = new WebSocket(`${wsOrigin3}/websocket/connection`)
+const wsOrigin4 = 'wss://izumra.ru'
+const ws = new WebSocket(`${wsOrigin4}/websocket/connection`)
 
 const loading = document.querySelector('.loading')
 const pages = {
@@ -29,19 +30,46 @@ switch (role) {
 		loading.classList.remove('active')
 		// =============
 
+		//добавление игры
+		const openPopupAddGameBtn = document.querySelector('.login__add-game-btn')
+		const addGamePopup = document.querySelector('.create-game-popup')
+		openPopupAddGameBtn.addEventListener('click', function() {
+			addGamePopup.classList.add('active')
+		})
+
+		const addGamePopup_closeBtn = addGamePopup.querySelector('.cancel-btn')
+		addGamePopup_closeBtn.addEventListener('click', function() {
+			addGamePopup.classList.remove('active')
+		})
+
+		//вход в игру
+		const loginPopup = document.querySelector('.login-popup')
+		const loginPopup_closeBtn = loginPopup.querySelector('.cancel-btn')
+		loginPopup_closeBtn.addEventListener('click', function () {
+			loginPopup.classList.remove('active')
+		})
+
 		// определение роли
-		const leadLoginBtn = document.querySelector('.login .lead')
-		leadLoginBtn.addEventListener('click', function () {
-			params.set('role', 'lead');
-			history.pushState(null, null, '?' + params.toString());
+		const roleSwitcher = loginPopup.querySelector('#role-switcher')
+		const loginBtn = loginPopup.querySelector('.login-btn')
+		loginBtn.addEventListener('click', function() {
+			if (roleSwitcher.checked) {
+				//view
+				params.set('role', 'view');
+				history.pushState(null, null, '?' + params.toString());
+			} else {
+				//lead
+				params.set('role', 'lead');
+				history.pushState(null, null, '?' + params.toString());
+			}
 			location.reload()
 		})
 
-		const viewLoginBtn = document.querySelector('.login .view')
-		viewLoginBtn.addEventListener('click', function () {
-			params.set('role', 'view');
-			history.pushState(null, null, '?' + params.toString());
-			location.reload()
+		document.addEventListener('click', function(evt) {
+			const target = evt.target
+			if (target.classList.contains('login-game')) {
+				loginPopup.classList.add('active')
+			}
 		})
 
 		break
